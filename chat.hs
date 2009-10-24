d0c224d3d2cf960b5ba09134f6bd1d742c3705cf
David Roundy <roundyd@physics.oregonstate.edu>
2009-10-24 07:42:27 -0400

    add unused, broken YAML support.

--- chat.hs
+++ chat.hs
@@ -1,9 +1,32 @@
 import TCP.Server ( startServer )
-import Control.Monad ( forever )
-import System.IO ( Handle, hPutStrLn, hGetLine, stdout )
+import YAML ( YAML, fromNode, parseYaml, showYaml )
 
+import Control.Monad ( forever )
+import System.IO ( Handle, hPutStrLn, hPutStr, hGetLine, hGetContents, stdout )
 import Control.Concurrent ( forkIO )
-import Control.Concurrent.Chan ( newChan, writeChan, readChan, dupChan )
+import Control.Concurrent.Chan ( Chan, newChan, writeChan, readChan, dupChan )
+
+yamlHandle :: (YAML i, YAML o) => Handle -> IO (Chan i, Chan o)
+yamlHandle h =
+    do i <- newChan
+       o <- newChan
+       forkIO $ do nodes <- parseYaml `fmap` hGetContents h
+                   let writeC n = case fromNode n of
+                                    Just x -> do putStrLn "got one"
+                                                 writeChan i x
+                                    Nothing -> do putStrLn "oopps"
+                                                  fail ("bad node "++show n)
+                   mapM_ writeC nodes
+       forkIO $ forever $ do x <- readChan o
+                             hPutStr h $ showYaml [x]
+       return (i,o)
+
+{-
+heSays :: Chan String -> String -> (String, String) -> IO ()
+heSays h me (him, statement)
+       | him == me = return ()
+       | otherwise = writeChan h (him++" says: "++statement)
+-}
 
 heSays :: Handle -> String -> (String, String) -> IO ()
 heSays h me (him, statement)
@@ -22,3 +45,14 @@ main = do c <- newChan
                                                 readChan c' >>= heSays h n
                                          forever $ do l <- hGetLine h
                                                       writeChan c' (n,l)
+
+         {-
+          startServer 12345 $ \n h ->
+              do (i,o) <- yamlHandle h
+                 c' <- dupChan c
+                 writeChan c' ("server",
+                               "Everyone, welcome "++n)
+                 forkIO $ forever $ readChan c' >>= heSays o n
+                 forever $ do l <- readChan i
+                              writeChan c' (n,l)
+-}
