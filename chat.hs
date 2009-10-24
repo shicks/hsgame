9d6e70a7c3c1ea0cafb5943a01b0640e886ba2cc
David Roundy <roundyd@physics.oregonstate.edu>
2009-10-24 13:04:44 -0400

    use chans with simple serialization class in chat.

--- chat.hs
+++ chat.hs
@@ -1,32 +1,42 @@
 import TCP.Server ( startServer )
-import YAML ( YAML, fromNode, parseYaml, showYaml )
 
 import Control.Monad ( forever )
-import System.IO ( Handle, hPutStrLn, hPutStr, hGetLine, hGetContents, stdout )
+import System.IO ( Handle, hPutStrLn, hGetLine, stdout )
 import Control.Concurrent ( forkIO )
 import Control.Concurrent.Chan ( Chan, newChan, writeChan, readChan, dupChan )
 
-yamlHandle :: (YAML i, YAML o) => Handle -> IO (Chan i, Chan o)
-yamlHandle h =
+class (Show a, Read a) => ShowRead a where
+    showLine :: a -> String
+    showLine = show
+    readLine :: String -> Maybe a
+    readLine x = do [(a,"")] <- Just $ reads x
+                    Just a
+class (Read c, Show c) => CharLike c where
+    toChar :: c -> Char
+    fromChar :: Char -> c
+instance CharLike Char where
+    toChar = id
+    fromChar = id
+instance CharLike c => ShowRead [c] where
+    showLine = map toChar
+    readLine = Just . map fromChar
+
+handle2chans :: (ShowRead i, ShowRead o) => Handle -> IO (Chan i, Chan o)
+handle2chans h =
     do i <- newChan
        o <- newChan
-       forkIO $ do nodes <- parseYaml `fmap` hGetContents h
-                   let writeC n = case fromNode n of
-                                    Just x -> do putStrLn "got one"
-                                                 writeChan i x
-                                    Nothing -> do putStrLn "oopps"
-                                                  fail ("bad node "++show n)
-                   mapM_ writeC nodes
+       forkIO $ forever $ do x <- hGetLine h
+                             case readLine x of
+                               Just a -> writeChan i a
+                               Nothing -> fail ("bad data: "++x)
        forkIO $ forever $ do x <- readChan o
-                             hPutStr h $ showYaml [x]
+                             hPutStrLn h $ showLine x
        return (i,o)
 
-{-
-heSays :: Chan String -> String -> (String, String) -> IO ()
-heSays h me (him, statement)
+heSaysC :: Chan String -> String -> (String, String) -> IO ()
+heSaysC h me (him, statement)
        | him == me = return ()
        | otherwise = writeChan h (him++" says: "++statement)
--}
 
 heSays :: Handle -> String -> (String, String) -> IO ()
 heSays h me (him, statement)
@@ -38,6 +48,7 @@ main = do c <- newChan
           forkIO $ forever $ do l <- getLine
                                 writeChan c ("server",l)
           forkIO $ forever $ readChan c >>= heSays stdout "server"
+          {-
           startServer 12345 $ \n h -> do c' <- dupChan c
                                          writeChan c' ("server",
                                                        "Everyone, welcome "++n)
@@ -45,14 +56,11 @@ main = do c <- newChan
                                                 readChan c' >>= heSays h n
                                          forever $ do l <- hGetLine h
                                                       writeChan c' (n,l)
-
-         {-
+          -}
           startServer 12345 $ \n h ->
-              do (i,o) <- yamlHandle h
+              do (i,o) <- handle2chans h
                  c' <- dupChan c
-                 writeChan c' ("server",
-                               "Everyone, welcome "++n)
-                 forkIO $ forever $ readChan c' >>= heSays o n
+                 writeChan c' ("server", "Everyone, welcome "++n)
+                 forkIO $ forever $ readChan c' >>= heSaysC o n
                  forever $ do l <- readChan i
                               writeChan c' (n,l)
--}
