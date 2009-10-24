e23f1152ccd52b400c5796c9bd8408aa15b1e10e
David Roundy <roundyd@physics.oregonstate.edu>
2009-10-23 22:45:59 -0400

    define the version number.

--- Setup.hs
+++ Setup.hs
@@ -1,7 +1,8 @@
 #!/usr/bin/runhaskell
 import Distribution.Franchise.V1
 
-main = build [] $ do autoVersion Numbered
+main = build [] $ do -- autoVersion Numbered
+                     version "0.0"
                      checkOnce "if we accept PostfixOperators" $
                                do ghcFlags ["-XPostfixOperators"]
                                   requireModule "Prelude"
