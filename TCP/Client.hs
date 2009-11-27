7696348265db05ce4775582c2ab015be1349214f
David Roundy <roundyd@physics.oregonstate.edu>
2009-11-27 09:54:05 -0500

    make Client API more similar to Server API.
    Tested-on: morland i686 GNU/Linux

--- TCP/Client.hs
+++ TCP/Client.hs
@@ -1,12 +1,24 @@
-module TCP.Client ( connectToServer ) where
+module TCP.Client ( runClientTCP, pureClient, ioClient ) where
 
 import Network ( connectTo, PortID(PortNumber) )
 import System.IO ( hSetBuffering, BufferMode(LineBuffering) )
-import TCP.Chan ( ShowRead, Input, Output, handle2io )
+import TCP.Chan ( ShowRead, Input, Output, handle2io, getInput, writeOutput )
 
-connectToServer :: (ShowRead a, ShowRead b) =>
-                   String -> Int -> IO (Input a, Output b)
-connectToServer hostname port =
+runClientTCP :: (ShowRead a, ShowRead b) =>
+                String -> Int -> Client a b -> IO ()
+runClientTCP hostname port (Client client) =
     do h <- connectTo hostname (PortNumber $ fromIntegral port)
        hSetBuffering h LineBuffering
-       handle2io h
+       (i,o) <- handle2io h
+       client i o
+
+data Client toclient toserver =
+    Client (Input toclient -> Output toserver -> IO ())
+
+ioClient :: (Input toclient -> Output toserver -> IO ())
+         -> Client toclient toserver
+ioClient = Client
+
+pureClient :: ([toclient] -> [toserver]) -> Client toclient toserver
+pureClient f = ioClient $ \i o -> do x <- getInput i
+                                     mapM_ (writeOutput o) $ f x
