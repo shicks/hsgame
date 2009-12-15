92a5074e11e4a7a29f63afc7134dc813002bf081
David Roundy <roundyd@physics.oregonstate.edu>
2009-12-15 07:21:09 -0500

    change dirServer to dirHandler and use Handlers.
    Tested-on: morland i686 GNU/Linux

--- HTTP/DirHandler.hs
+++ HTTP/DirHandler.hs
@@ -1,7 +1,15 @@
-module HTTP.DirHandler ( dirServer ) where
+module HTTP.DirHandler ( dirHandler ) where
 
 import HTTP.Response ( Response, error404 )
 import HTTP.LoginServer ( Agent )
+import HTTP.Handlers ( Handler(..), useHandler )
+
+dirHandler :: [(Maybe String, IO Handler)]
+           -> IO (Agent -> [String] -> [(String,String)] -> IO Response)
+dirHandler spec0 = do spec <- mapM makeHandler spec0
+                      return (dirServer spec)
+    where makeHandler (ms, mkh) = do h <- mkh >>= useHandler
+                                     return (ms, h)
 
 dirServer :: [(Maybe String,        -- "directory name"
                (Agent -> [String] -> [(String,String)] -> IO Response))]
--- httpd.hs
+++ httpd.hs
@@ -4,20 +4,15 @@ import HTTP.Server ( httpServer, pure )
 import HTTP.FileServer ( fileServer )
 import HTTP.ChatServer ( chatHandler )
 import HTTP.LoginServer ( LoginMessage(SendMessage), loginServer, loginThread )
-import HTTP.DirHandler ( dirServer )
-import HTTP.Handlers ( useHandler )
--- import Dominion.Server ( dominionServer, dominionThread )
+import HTTP.DirHandler ( dirHandler )
+
 import TCP.Chan ( pipe, writeOutput )
 import Control.Concurrent ( forkIO )
 
 main :: IO ()
-main = do (li,lo) <- pipe -- we could make servers an IO (.. -> IO Response)
+main = do (li,lo) <- pipe
           forkIO $ loginThread li []
           let sendM a s = writeOutput lo $ SendMessage a s
-          chat <- chatHandler "chat" sendM >>= useHandler
-          silly <- chatHandler "silly" sendM >>= useHandler
-          -- forkIO $ dominionThread ddi []
-          httpServer 8081 $ pure $ loginServer lo fileServer $
-                     dirServer [(Just "chat",chat),
-                                (Just "silly", silly)]
-                                --(Just "dominion",dominionServer dso)]
+          dh <- dirHandler [(Just "chat", chatHandler "chat" sendM),
+                            (Just "silly", chatHandler "silly" sendM)]
+          httpServer 8081 $ pure $ loginServer lo fileServer dh
